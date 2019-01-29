393c9dcc89427fb044c876fe48bea8bfa92b641c
Mehdi <hassanpourmehdi96@gmail.com>
2019-01-29 03:57:33 +0330

    merged

--- code_generation.py
+++ code_generation.py
@@ -1,12 +1,12 @@
 ss = []
-add_flag = 0
-PB_SIZE = 10
-PB = [] * PB_SIZE
+PB = []
 PB_counter = 0
 whiles = []
 address = 10000
+
 #TODO remove symbol table after merge
 symbol_table = []
+
 
 def gettemp():
     global address
@@ -17,11 +17,14 @@ def gettemp():
 def findaddr(input):
     pass
 
+
 def top(stack):
     return len(stack) - 1
 
+
 def convertToStr(func, par1='', par2='', par3=''):
     return '(' + func + ', ' + str(par1) + ', ' + str(par2) + ', ' + str(par3) + ')'
+
 
 def code_generation(action, input):
     global PB_counter
@@ -29,13 +32,13 @@ def code_generation(action, input):
     global scope
     if action == "#PUSH_SS":
         ss.append(input)
-
+        return
     elif action == "#ADDSUB":
         t = gettemp()
         if ss[top(ss) - 1] == "+":
-            PB[PB_counter] = (ADD, ss[top(ss)], ss[top(ss) - 1], t)
+            PB.append(convertToStr("ADD", symbol_table[ss[top(ss)]].values()[0], symbol_table[ss[top(ss) - 1]].values()[0], t))
         elif ss[top(ss) - 1] == "-":
-            PB[PB_counter] = (SUB, ss[top(ss)], ss[top(ss) - 1], t)
+            PB.append(convertToStr("SUB", symbol_table[ss[top(ss)]].values()[0], symbol_table[ss[top(ss) - 1]].values()[0], t))
         PB_counter += 1
         for i in range(3):
             ss.pop()
@@ -43,7 +46,9 @@ def code_generation(action, input):
         return
     elif action == "MULT":
         t = gettemp()
-        PB[PB_counter] = (MULT, ss[top(ss)], ss[top(ss) - 1], t)
+        # symbol_table[ss[top(ss)]]
+        PB.append(convertToStr("MULT", symbol_table[ss[top(ss)]].values()[0], symbol_table[ss[top(ss) - 1]].values()[0], t))
+        # PB[PB_counter] = (MULT, ss[top(ss)], ss[top(ss) - 1], t)
         PB_counter += 1
         ss.pop()
         ss.pop()
@@ -72,12 +77,12 @@ def code_generation(action, input):
         arr_dict = symbol_table[ss.pop()].values()[0]
         arr_dict['addr'] = address
         address += 32
-        PB[PB_counter] = '(ASSIGN, #' + str(address) + ',' + str(arr_dict['addr']) + ', )'
+        PB.append('(ASSIGN, #' + str(address) + ',' + str(arr_dict['addr']) + ', )')
         PB_counter += 1
         address += 32 * input
         return
     elif action == "#INC_SCOPE":
-        scope +=1
+        scope += 1
         return
     elif action == "#PAR_ADDR":
         param_dict = symbol_table[input].values()[0]
@@ -92,12 +97,13 @@ def code_generation(action, input):
         return
     elif action == "#ASSIGN_RET":
         fun_dict = symbol_table[ss[top(ss) - 1]].values()[0]
-        PB[PB_counter] = '(ASSIGN, #' + str(ss.pop()) + ',' + str(fun_dict['ret_val']) + ', )'
+        PB.append('(ASSIGN, #' + str(ss.pop()) + ',' + str(fun_dict['ret_val']) + ', )')
         PB_counter += 1
         return
     elif action == "#JMP_CALLER":
         fun_dict = symbol_table[ss[top(ss)]].values()[0]
-        PB[PB_counter] = '(JP, ' + str(fun_dict['ret_addr']) + ', , )'
+        PB.append('(JP, ' + str(fun_dict['ret_addr']) + ', , )')
+        PB_counter += 1
         return
     elif action == "#POP_SS":
         ss.pop()
@@ -112,20 +118,28 @@ def code_generation(action, input):
     elif action == "#SAVE_CONTINUE":
         whiles.append(([PB_counter], []))
         return
+    elif action == "#SAVE_SWITCH":
+        whiles.append(([], []))
+        return
+    elif action == "#END_SWITCH":
+        last_switch = whiles[-1][1]
+        for i in last_switch:
+            PB[i] = convertToStr("JP", PB_counter)
+        whiles.pop()
     elif action == "#WHILE":
-        PB[ss[top(ss)]] = (JPF, ss[top(ss) - 1], PB_counter + 1, )
-        PB[PB_counter] = (JP, ss[top(ss) - 2], ,)
+        PB[ss[top(ss)]] = convertToStr("JPF", ss[top(ss) - 1], PB_counter + 1)
+        PB.append(convertToStr("JP", ss[top(ss) - 2]))
         PB_counter += 1
         last_loop_breaks = whiles[-1][1]
         for i in last_loop_breaks:
-            PB[i] = (JP, PB_counter, ,)
+            PB[i] = convertToStr("JP", PB_counter)
         whiles.pop()
         for i in range(3):
             ss.pop()
         return
     elif action == "#JMP_BEGIN":
         # PB[PB_counter] = (JP, ss.get(ss.top))
-        PB[PB_counter] = (JP, whiles[-1][0][-1])
+        PB.append(convertToStr("JP", whiles[-1][0][-1]))
         PB_counter += 1
         return
     elif action == "#JMP_END":
@@ -133,26 +147,26 @@ def code_generation(action, input):
         PB_counter += 1
         return
     elif action == "#JPF_SAVE":
-        PB[ss[top(ss)]] = (JPF, ss[top(ss)-1], PB_counter+1)
+        PB[ss[top(ss)]] = convertToStr("JPF", ss[top(ss)-1], PB_counter+1)
         ss.pop()
         ss.pop()
         ss.append(PB_counter)
         PB_counter += 1
         return
     elif action == "#JP":
-        PB[ss[top(ss)]] = (JP, PB_counter, ,)
+        PB[ss[top(ss)]] = convertToStr("JP", PB_counter)
         ss.pop()
         return
     elif action == "#JPT_SAVE":
         pass
     elif action == "#COMPARE_CASE":
         t = gettemp()
-        PB[PB_counter] = (EQ, int(input), ss[top(ss) - 1], t)
+        PB.append(convertToStr("EQ", int(input), ss[top(ss) - 1], t))
         PB_counter += 1
         ss.append(t)
         return
     elif action == "#JPF":
-        PB[ss[top(ss)]] = JPF(ss[top(ss) - 1], PB_counter, ,)
+        PB[ss[top(ss)]] = convertToStr("JPF", ss[top(ss) - 1], PB_counter)
         ss.pop()
         ss.pop()
         return
@@ -162,9 +176,9 @@ def code_generation(action, input):
     elif action == "#RELOP":
         t = gettemp()
         if ss[top(ss) - 1] == "<":
-            PB[PB_counter] = (LT, ss[top(ss) - 2], ss[top(ss)], t)
+            PB.append(convertToStr("LT", ss[top(ss) - 2], ss[top(ss)], t))
         elif ss[top(ss) - 1] == "==":
-            PB[PB_counter] = (EQ, ss[top(ss) - 2], ss[top(ss)], t)
+            PB.append(convertToStr("EQ", ss[top(ss) - 2], ss[top(ss)], t))
         PB_counter += 1
         for i in range(3):
             ss.pop()
